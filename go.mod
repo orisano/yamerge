8828375a422780e319e2aace4e308b9fdf0c01de
Nao Yonashiro <owan.orisano@gmail.com>
2020-07-30 21:14:25 +0900

    refactor: use goccy/go-yaml

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/orisano/yamerge
 go 1.13
 
 require (
-	github.com/imdario/mergo v0.3.5
-	gopkg.in/yaml.v2 v2.2.1
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/goccy/go-yaml v1.8.0
+	github.com/imdario/mergo v0.3.10
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	golang.org/x/sys v0.0.0-20200728102440-3e129f6d46b1 // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
 )
--- main.go
+++ main.go
@@ -1,39 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/goccy/go-yaml"
 	"github.com/imdario/mergo"
-	yaml "gopkg.in/yaml.v2"
 )
+
+func main() {
+	log.SetPrefix("yamerge: ")
+	log.SetFlags(0)
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	v := make(map[string]interface{})
+	for _, p := range os.Args[1:] {
+		x, err := loadYAML(p)
+		if err != nil {
+			return fmt.Errorf("load yaml: %w", err)
+		}
+		if err := mergo.Map(&v, x, mergo.WithAppendSlice); err != nil {
+			return fmt.Errorf("merge yaml: %w", err)
+		}
+	}
+	if err := yaml.NewEncoder(os.Stdout).Encode(v); err != nil {
+		return fmt.Errorf("encode yaml: %w", err)
+	}
+	return nil
+}
 
 func loadYAML(filePath string) (map[string]interface{}, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("open yaml: %w", err)
 	}
 	defer f.Close()
 
 	x := make(map[string]interface{})
 	if err := yaml.NewDecoder(f).Decode(&x); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("decode yaml: %w", err)
 	}
 	return x, nil
-}
-
-func main() {
-	v := make(map[string]interface{})
-	for _, p := range os.Args[1:] {
-		x, err := loadYAML(p)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := mergo.Map(&v, x, mergo.WithAppendSlice); err != nil {
-			log.Fatal(err)
-		}
-	}
-	if err := yaml.NewEncoder(os.Stdout).Encode(v); err != nil {
-		log.Fatal(err)
-	}
 }
